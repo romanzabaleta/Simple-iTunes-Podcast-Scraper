68405f8a9fbd1739e4cc339515052ec8ee3ae088
romanzabaleta <36675704+romanzabaleta@users.noreply.github.com>
2018-04-04 14:27:47 -0400

    Began adding a data gathering function
    The data gathering function does not work yet

--- Podcast Data Scraper.py
+++ Podcast Data Scraper.py
@@ -1,6 +1,8 @@
 import pattern3.web as web
 from bs4 import BeautifulSoup
 import re
+import pandas as pd
+from progressbar import ProgressBar
 
 ###Find links to genre pages with all podcast links
 #URL page with all genres of podcasts
@@ -44,3 +46,44 @@ podcast_links = []
 for genre_link in genre_links:
     all_links = scrape_links(genre_link)
     podcast_links.extend(recognize_pattern(all_links, pattern2))
+
+columns= ['Name', 'Genre ID', 'Episode Count', 'Episode Durations','Rating Value', 'Rating Volume','Description', 'Release Dates']
+podcast_df = pd.DataFrame([[0, 0, 0, 0, 0, 0, 0, 0]], columns = columns)
+
+def get_data(webp):
+    try:
+        url = web.URL(webp)
+        bs = BeautifulSoup(url.download(cached = False))
+        titles = bs.find('div', id='title')
+        if titles is not None:
+            title = titles.find('hi').getText()
+
+        my_row = [title, genreid, eount, edurations, rValue, rvolume, description, released]
+        my_row_pd = pd.DataFrame([my_row], columns = columns)  
+        df_podcast = df_podcast.append(my_row_pd, ignore_index=True)
+
+    except: 
+            return None 
+
+print(len(podcast_links))
+
+pbar = ProgressBar()
+for link in pbar(podcast_links[360]):
+    get_data(link)
+print(podcast_df)
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
