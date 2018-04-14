e2c088a13628d64c8c0fb6596e4ad4d6e80037d9
Roman Antonio Zabaleta <36675704+romanzabaleta@users.noreply.github.com>
2018-04-14 17:33:00 -0400

    changed variabe names

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import linkfetch
 
-pa = ['https://itunes.apple.com/us/genre/podcasts/id26?mt=2',
+itunesl = ['https://itunes.apple.com/us/genre/podcasts/id26?mt=2',
       'https://itunes.apple.com/us/genre/podcasts-',
       'https://itunes.apple.com/us/podcast/'
       ]
 
 
-links = linkfetch.find_links(pa[0],pa[1], pa[2])
-print(len(links))
+itunes_links = linkfetch.find_links(itunesl[0],itunesl[1], itunesl[2])
+print(len(itunes_links))
+
