6df804622621cee243f303f8ba76a991cba3b415
romanzabaleta <36675704+romanzabaleta@users.noreply.github.com>
2018-08-11 22:19:29 -0400

    0.3
    Working scrapper

--- main.py
+++ main.py
@@ -3,25 +3,37 @@ import it_getdata as get
 import pandas as pd
 import genre_id_reader as gen 
 import podatabase
+import csv
 
 print('running...')
 
-itunesl = ['https://itunes.apple.com/us/genre/podcasts/id26?mt=2',
-      'https://itunes.apple.com/us/genre/podcasts-',
-      'https://itunes.apple.com/us/podcast/']
+#itunesl = ['https://itunes.apple.com/us/genre/podcasts/id26?mt=2',
+#      'https://itunes.apple.com/us/genre/podcasts-',
+#      'https://itunes.apple.com/us/podcast/']
 
-itunes_links = linkfetch.find_links(itunesl[0],itunesl[1], itunesl[2])
+#itunes_links = linkfetch.find_links(itunesl[0],itunesl[1], itunesl[2])
+with open('csvfile.csv', 'r') as output:
+    reader = csv.reader(output, delimiter='\n')
+    itunes_links = list(reader)
+
 print(len(itunes_links))
-num =len(itunes_links)-1
-print(itunes_links[num])
+#dfcsv = pd.read_csv('csvfile.csv', delimiter='\n')
+#itunes_links = dfcsv.to_dict().values()
+#print(len(itunes_links))
 
-columns = ['Name', 'Rating Volume', 'Rating', 'Genre', 'Description']
+columns = ['Name', 'Rating_Volume', 'Rating', 'Genre', 'Description']
 podcast_df = pd.DataFrame(columns=columns)
-
-for i in range(1000,1050):
-	ok = get.get_data(itunes_links[i], podcast_df, columns)
+#1500 to 1830
+for i in range(2000,2260):
+	ok = get.get_data(itunes_links[i][0], podcast_df, columns)
 	podcast_df = podcast_df.append(ok)
 	print(i)
-	print(itunes_links[i])
+	print(itunes_links[i][0])
 
-podatabase.podb(podcast_df)
+podatabase.updatepodb(podcast_df)
+#podatabase.newpodb(podcast_df)
+
+#with open('csvfile.txt', "w") as output:
+#    writer = csv.writer(output, lineterminator='\n')
+#    for val in itunes_links:
+#        writer.writerow([val])
--- podatabase.py
+++ podatabase.py
@@ -1,11 +1,21 @@
 import sqlite3
 from pandas.io import sql
 
-def podb(poddf):
-	podb = sqlite3.connect('itunes_podcast.db')
-	c = podb.cursor()
-	sql.to_sql(poddf, name='poddf', con=podb)
-	p2 = sql.read_sql('select * from poddf', podb)
-	podb.commit()
-	podb.close()
+def newpodb(poddf):
+	conn = sqlite3.connect('itunes_podcast.db')
+	c = conn.cursor()
+	sql.to_sql(poddf, name='poddf', con=conn)
+	p2 = sql.read_sql('select * from poddf', conn)
+	conn.commit()
+	conn.close()
 	print(p2)
+
+def updatepodb(poddf):
+	conn = sqlite3.connect('itunes_podcast.db')
+	c = conn.cursor()
+	poddf.to_sql('poddf', conn, if_exists='append')
+	p2 = sql.read_sql('select * from poddf', conn)
+	conn.commit()
+	conn.close()
+	print(p2)
+
